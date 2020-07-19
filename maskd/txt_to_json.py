adee8eb23c61c509995c7ec70388c8daeb6666e2
Bharath A <41417337+BharathA14@users.noreply.github.com>
2020-07-19 17:08:12 +0530

    Yolo_v5 files

---
+++ maskd/txt_to_json.py
@@ -0,0 +1,27 @@
+import glob
+import json
+
+result = []
+
+with open("dataset/test.json") as f:
+  data = json.load(f)
+image_id = dict()
+for i in data['images']:
+    image_id[i['file_name'][:-4]] = i['id']  #[:-4] to remove extensions
+# print(image_id)
+for i in (glob.glob("output/*.txt")):
+#    print(i[7:])
+    open_file = open(i,'r')
+    for j in open_file.readlines():
+        arr=j.split(" ")
+        arr[-1] = arr[-1][:-1]
+        new_result = dict()
+        new_result['image_id'] = int(image_id[i[7:-4]]) #to read outputfile and remove extensions
+        new_result['category_id'] = int(arr[0])
+        new_result['score'] = float(arr[1])
+        new_result['bbox'] = [float(arr[i]) for i in range(2,6)]
+        result.append(new_result)
+out_file = open("result.json", "w")  
+    
+json.dump(result, out_file, indent = 6)  
+    
