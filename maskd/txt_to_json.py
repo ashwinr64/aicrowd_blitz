ba8a563b3becef4186b16444b9020e7e8f1a812e
Bharath A <41417337+BharathA14@users.noreply.github.com>
2020-07-26 09:55:06 +0530

    Add files via upload

--- maskd/txt_to_json.py
+++ maskd/txt_to_json.py
@@ -1,3 +1,6 @@
+#FOLDER NAMED DATASET MUST CONTAIN THE TEST.JSON
+#FOLDER NAMED OUTPUT MUST CONTAIN ALL THE TXT FILES FROM YOLO
+
 import glob
 import json
 
@@ -7,20 +10,22 @@ with open("dataset/test.json") as f:
   data = json.load(f)
 image_id = dict()
 for i in data['images']:
-    image_id[i['file_name'][:-4]] = i['id']  #[:-4] to remove extensions
+    image_id[i['file_name'].split('.')[0]] = i['id']  #[:-4] to remove extensions
 # print(image_id)
 for i in (glob.glob("output/*.txt")):
 #    print(i[7:])
     open_file = open(i,'r')
     for j in open_file.readlines():
         arr=j.split(" ")
-        arr[-1] = arr[-1][:-1]
+        print(arr[-1].split('\n'))
+        arr[-1] = arr[-1].split('\n')[0]
         new_result = dict()
-        new_result['image_id'] = int(image_id[i[7:-4]]) #to read outputfile and remove extensions
+        new_result['image_id'] = int(image_id[i[7:].split('.')[0]]) #to read outputfile and remove extensions
         new_result['category_id'] = int(arr[0])
         new_result['score'] = float(arr[1])
         new_result['bbox'] = [float(arr[i]) for i in range(2,6)]
         result.append(new_result)
+# print(result)
 out_file = open("result.json", "w")  
     
 json.dump(result, out_file, indent = 6)  
